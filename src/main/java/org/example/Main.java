492222df316c3740e3aabb8407ba2f89f6e8b904
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2023-01-12 20:07:23 +0100

    Part 2 done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,15 @@ package org.example;
 
 import org.example.config.ComponentScanConfig;
 import org.example.data_access.StudentDao;
+import org.example.userInput.UserInputService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+
         StudentDao studentDao = context.getBean(StudentDao.class);
+        UserInputService userInputService =context.getBean(UserInputService.class);
     }
 }
