88d70fcc38c07da345f121ad791d1c41ea9fd906
Mohammad Alissa <mohammad.alissa95@hotmail.com>
2023-01-12 19:48:06 +0100

    Part 1 done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.config.ComponentScanConfig;
+import org.example.data_access.StudentDao;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        StudentDao studentDao = context.getBean(StudentDao.class);
+    }
+}
